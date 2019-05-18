5c0d358547a69e67d7182d5f0b583356f245f55a
MangarSo <soumya.mangaraj@adp.com>
2019-05-18 13:50:12 +0530

    Completed the module

--- jobs/app.py
+++ jobs/app.py
@@ -23,7 +23,7 @@ def execute_sql(sql, values=(), commit=False, single=False):
 @app.teardown_appcontext
 def close_connection(exception):
     connection = getattr(g, '_connection', None);
-    if connection not in None:
+    if connection != None:
         connection.close();
 
 @app.route('/')
